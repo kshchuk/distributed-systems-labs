419c585c4772ced5314ce725ef7ee3910e10d240
kshchuk <kshchuk@gmail.com>
2023-11-10 20:23:27 +0200

    Created XSD validator & Error Handler

---
+++ lab-7-XML/src/main/java/org/example/xml/validator/ValidatorXSD.java
@@ -0,0 +1,34 @@
+package org.example.validator;
+
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+
+public class ValidatorXSD {
+    private Schema schema;
+
+    public ValidatorXSD(String xsd) throws SAXException {
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        schema = schemaFactory.newSchema(new File(xsd));
+    }
+    public boolean isValid(String xml) {
+        try {
+            validate(xml);
+        } catch (SAXException | IOException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public void validate(String xml) throws SAXException, IOException {
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        Validator validator = schema.newValidator();
+        validator.validate(new StreamSource(new File(xml)));
+    }
+}
