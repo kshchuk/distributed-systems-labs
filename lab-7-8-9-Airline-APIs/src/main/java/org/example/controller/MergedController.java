0cbc42a19d0e6cf7ae7a19c6b6b3e83ae2810944
kshchuk <kshchuk@gmail.com>
2023-12-07 00:12:50 +0200

    implemented RMI technology

--- lab-7-8-9-Airline-APIs/src/main/java/org/example/controller/MergedController.java
+++ lab-7-8-9-Airline-APIs/src/main/java/org/example/controller/MergedController.java
@@ -3,17 +3,17 @@ package org.example.controller;
 import org.example.dto.AirlineDto;
 import org.example.dto.FlightDto;
 
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.util.List;
 import java.util.UUID;
 
-public interface MergedController {
-    AirlineDto createAirline(AirlineDto airlineDto) throws Exception;
+public interface MergedController extends Remote {
+    AirlineDto createAirline(AirlineDto airlineDto) throws RemoteException;
     AirlineDto getAirline(UUID id) throws Exception;
-    AirlineDto getAirline(String name) throws Exception;
     void updateAirline(AirlineDto airlineDto) throws Exception;
     boolean deleteAirline(UUID id) throws Exception;
-    List<AirlineDto> findAllAirlines() throws Exception;
-
+    List<AirlineDto> findAllAirlines() throws RemoteException;
     FlightDto createFlight(FlightDto flightDto) throws Exception;
     FlightDto getFlight(UUID id) throws Exception;
     void updateFlight(FlightDto flightDto) throws Exception;
