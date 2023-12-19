efac16b603b5ddd1165ac56acdce0099f75765c6
kshchuk <kshchuk@gmail.com>
2023-12-19 13:06:13 +0200

    implemented socket server

--- ekz/Ekzamen/src/main/java/org/example/socket/client/Client.java
+++ ekz/Ekzamen/src/main/java/org/example/socket/client/Client.java
@@ -1,5 +1,7 @@
 package org.example.socket.client;
 
+import org.example.dto.EmailContactDto;
+import org.example.dto.PhoneContactDto;
 import org.example.dto.Request;
 
 import java.io.IOException;
@@ -7,6 +9,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.Scanner;
+import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -73,16 +76,151 @@ public class Client {
         var scanner = new Scanner(System.in);
 
         while (true) {
-
+            System.out.println("1. Create email contact");
+            System.out.println("2. Create phone contact");
+            System.out.println("3. Read email contact");
+            System.out.println("4. Read phone contact");
+            System.out.println("5. Update email contact");
+            System.out.println("6. Update phone contact");
+            System.out.println("7. Delete email contact");
+            System.out.println("8. Delete phone contact");
+            System.out.println("9. Filter email contact by first name");
+            System.out.println("10. Filter phone contact by first name");
+            System.out.println("11. Filter email contact by last name");
+            System.out.println("12. Filter phone contact by last name");
+            System.out.println("13. Filter email contact by First name and last name");
+            System.out.println("14. Filter phone contact by First name and last name");
+            System.out.println("15. Exit");
 
             var option = scanner.nextInt();
 
-//            try {
-//                switch (option) {
-//
-//            } catch (Exception ex) {
-//                System.out.println(ex.getMessage());
-//            }
+            try {
+                switch (option) {
+                    case 1 -> {
+                        var emailContactDto = new EmailContactDto();
+                        System.out.println("Enter first name:");
+                        emailContactDto.setFirstName(scanner.next());
+                        System.out.println("Enter last name:");
+                        emailContactDto.setLastName(scanner.next());
+                        System.out.println("Enter email:");
+                        emailContactDto.setEmail(scanner.next());
+
+                        var request = new Request(Request.RequestMethod.POST, "/email", emailContactDto);
+                        client.SendRequest(request);
+                    }
+                    case 2 -> {
+                        var phoneContactDto = new PhoneContactDto();
+                        System.out.println("Enter first name:");
+                        phoneContactDto.setFirstName(scanner.next());
+                        System.out.println("Enter last name:");
+                        phoneContactDto.setLastName(scanner.next());
+                        System.out.println("Enter phone:");
+                        phoneContactDto.setPhone(scanner.next());
+
+                        var request = new Request(Request.RequestMethod.POST, "/phone", phoneContactDto);
+                        client.SendRequest(request);
+                    }
+                    case 3 -> {
+                        System.out.println("Enter id:");
+                        var id = scanner.next();
+                        var request = new Request(Request.RequestMethod.GET, "/email/" + id, null);
+                        client.SendRequest(request);
+                    }
+                    case 4 -> {
+                        System.out.println("Enter id:");
+                        var id = scanner.next();
+                        var request = new Request(Request.RequestMethod.GET, "/phone/" + id, null);
+                        client.SendRequest(request);
+                    }
+                    case 5 -> {
+                        var emailContactDto = new EmailContactDto();
+                        System.out.println("Enter id:");
+                        emailContactDto.setId(UUID.fromString(scanner.next()));
+                        System.out.println("Enter first name:");
+                        emailContactDto.setFirstName(scanner.next());
+                        System.out.println("Enter last name:");
+                        emailContactDto.setLastName(scanner.next());
+                        System.out.println("Enter email:");
+                        emailContactDto.setEmail(scanner.next());
+
+                        var request = new Request(Request.RequestMethod.PUT, "/email", emailContactDto);
+                        client.SendRequest(request);
+                    }
+                    case 6 -> {
+                        var phoneContactDto = new PhoneContactDto();
+                        System.out.println("Enter id:");
+                        phoneContactDto.setId(UUID.fromString(scanner.next()));
+                        System.out.println("Enter first name:");
+                        phoneContactDto.setFirstName(scanner.next());
+                        System.out.println("Enter last name:");
+                        phoneContactDto.setLastName(scanner.next());
+                        System.out.println("Enter phone:");
+                        phoneContactDto.setPhone(scanner.next());
+
+                        var request = new Request(Request.RequestMethod.PUT, "/phone", phoneContactDto);
+                        client.SendRequest(request);
+                    }
+                    case 7 -> {
+                        System.out.println("Enter id:");
+                        var id = scanner.next();
+                        var request = new Request(Request.RequestMethod.DELETE, "/email/" + id, null);
+                        client.SendRequest(request);
+                    }
+                    case 8 -> {
+                        System.out.println("Enter id:");
+                        var id = scanner.next();
+                        var request = new Request(Request.RequestMethod.DELETE, "/phone/" + id, null);
+                        client.SendRequest(request);
+                    }
+                    case 9 -> {
+                        System.out.println("Enter first name:");
+                        var firstName = scanner.next();
+                        var request = new Request(Request.RequestMethod.GET, "/email/byName/" + firstName, null);
+                        client.SendRequest(request);
+                    }
+                    case 10 -> {
+                        System.out.println("Enter first name:");
+                        var firstName = scanner.next();
+                        var request = new Request(Request.RequestMethod.GET, "/phone/byName/" + firstName, null);
+                        client.SendRequest(request);
+                    }
+                    case 11 -> {
+                        System.out.println("Enter last name:");
+                        var lastName = scanner.next();
+                        var request = new Request(Request.RequestMethod.GET, "/email/byLastName/" + lastName, null);
+                        client.SendRequest(request);
+                    }
+                    case 12 -> {
+                        System.out.println("Enter last name:");
+                        var lastName = scanner.next();
+                        var request = new Request(Request.RequestMethod.GET, "/phone/byLastName/" + lastName, null);
+                        client.SendRequest(request);
+                    }
+                    case 13 -> {
+                        System.out.println("Enter first name:");
+                        var firstName = scanner.next();
+                        System.out.println("Enter last name:");
+                        var lastName = scanner.next();
+                        var request = new Request(Request.RequestMethod.GET, "/email/byFullName/" + firstName + " " + lastName, null);
+                        client.SendRequest(request);
+                    }
+                    case 14 -> {
+                        System.out.println("Enter first name:");
+                        var firstName = scanner.next();
+                        System.out.println("Enter last name:");
+                        var lastName = scanner.next();
+                        var request = new Request(Request.RequestMethod.GET, "/phone/byFullName/" + firstName + " " + lastName, null);
+                        client.SendRequest(request);
+                    }
+                    case 15 -> {
+                        client.Stop();
+                        System.exit(0);
+                    }
+                }
+
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
         }
     }
 }
--- ekz/Ekzamen/src/main/java/org/example/socket/server/handler/RequestHandler.java
+++ ekz/Ekzamen/src/main/java/org/example/socket/server/handler/RequestHandler.java
@@ -2,8 +2,12 @@ package org.example.socket.server.handler;
 
 import org.example.controller.EmailContactController;
 import org.example.controller.PhoneContactController;
+import org.example.dto.EmailContactDto;
+import org.example.dto.PhoneContactDto;
 import org.example.dto.Request;
 import org.example.dto.Response;
+
+import java.util.UUID;
 
 public class RequestHandler {
     private final EmailContactController emailContactController;
@@ -17,21 +21,62 @@ public class RequestHandler {
     public Response handle(Request request) throws Exception {
         switch (request.getMethod()) {
             case GET:
+                if (request.getPath().startsWith("/email/")) {
+                    var id = request.getPath().substring(7);
+                    return new Response(Response.ResponseStatus.SUCCESS, this.emailContactController.get(UUID.fromString(id)));
+                } else if (request.getPath().startsWith("/phone/")) {
+                    var id = request.getPath().substring(7);
+                    return new Response(Response.ResponseStatus.SUCCESS, this.phoneContactController.get(UUID.fromString(id)));
+                } else if (request.getPath().startsWith("/email/byName/")) {
+                    var name = request.getPath().substring(13);
+                    return new Response(Response.ResponseStatus.SUCCESS, this.emailContactController.findByFirstName(name));
+                } else if (request.getPath().startsWith("/phone/byName/")) {
+                    var name = request.getPath().substring(13);
+                    return new Response(Response.ResponseStatus.SUCCESS, this.phoneContactController.findAllByFirstName(name));
+                } else if (request.getPath().startsWith("/email/byLastName/")) {
+                    var name = request.getPath().substring(15);
+                    return new Response(Response.ResponseStatus.SUCCESS, this.emailContactController.findByLastName(name));
+                } else if (request.getPath().startsWith("/phone/byLastName/")) {
+                    var name = request.getPath().substring(15);
+                    return new Response(Response.ResponseStatus.SUCCESS, this.phoneContactController.findAllByLastName(name));
+                } else if (request.getPath().startsWith("/email/byFullName/")) {
+                    var name = request.getPath().substring(15);
+                    var names = name.split(" ");
+                    return new Response(Response.ResponseStatus.SUCCESS, this.emailContactController.findByFullName(names[0], names[1]));
+                } else if (request.getPath().startsWith("/phone/byFullName/")) {
+                    var name = request.getPath().substring(15);
+                    var names = name.split(" ");
+                    return new Response(Response.ResponseStatus.SUCCESS, this.phoneContactController.findAllByFullName(names[0], names[1]));
+                }
             case DELETE:
-            case PATCH:
-                break;
-            case HEAD:
-                break;
-            case OPTIONS:
-                break;
-            case TRACE:
-                break;
-            case CONNECT:
-                break;
+                if (request.getPath().startsWith("/email/")) {
+                    var id = request.getPath().substring(7);
+                    this.emailContactController.delete(UUID.fromString(id));
+                    return new Response(Response.ResponseStatus.SUCCESS, "Deleted");
+                } else if (request.getPath().startsWith("/phone/")) {
+                    var id = request.getPath().substring(7);
+                    this.phoneContactController.delete(UUID.fromString(id));
+                    return new Response(Response.ResponseStatus.SUCCESS, "Deleted");
+                }
+            case POST:
+                if (request.getPath().startsWith("/email/")) {
+                    var dto = this.emailContactController.create((EmailContactDto) request.getBody());
+                    return new Response(Response.ResponseStatus.SUCCESS, dto);
+                } else if (request.getPath().startsWith("/phone/")) {
+                    var dto = this.phoneContactController.create((PhoneContactDto) request.getBody());
+                    return new Response(Response.ResponseStatus.SUCCESS, dto);
+                }
+            case PUT:
+                if (request.getPath().startsWith("/email/")) {
+                    this.emailContactController.update((EmailContactDto) request.getBody());
+                    return new Response(Response.ResponseStatus.SUCCESS, "Updated");
+                } else if (request.getPath().startsWith("/phone/")) {
+                    this.phoneContactController.update((PhoneContactDto) request.getBody());
+                    return new Response(Response.ResponseStatus.SUCCESS, "Updated");
+                }
             default:
                 throw new Exception("Unknown request method");
 
         }
-        return new Response(Response.ResponseStatus.ERROR, "Not found");
     }
 }
